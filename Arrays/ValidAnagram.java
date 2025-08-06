cc90dbec71957f17deb608151bd289d188de80ac
PALEGARU ROOPESH NAIDU <palegariroopeshnaidu@gmail.com>
2025-08-06 19:04:13 +0530

    Valid Anagram it returns either it is anagram or not

---
+++ Arrays/ValidAnagram.java
@@ -0,0 +1,28 @@
+package Arrays;
+import java.util.Arrays;
+public class ValidAnagram {
+    public static boolean isAnagram(String s, String t) {
+        if(s.length()!= t.length()){
+            return false;
+        }
+        char[] sArray = s.toCharArray();
+        char[] tArray = t.toCharArray();
+
+        Arrays.sort(sArray);
+        Arrays.sort(tArray);
+
+
+        return Arrays.equals(sArray,tArray);
+    }
+    public static void main(String[] args) {
+        String s = "anagram";
+        String t = "nagaram";
+        if(isAnagram(s,t)){
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }
+
+    }
+}
