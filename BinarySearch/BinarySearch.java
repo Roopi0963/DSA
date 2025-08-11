31f2a061780fb4b8fee68a16ce73e33bcd9f3ff1
PALEGARU ROOPESH NAIDU <palegariroopeshnaidu@gmail.com>
2025-08-11 21:45:42 +0530

    Implement BinarySearch
    Implemented the binary search technique to which returns the index of the element

---
+++ BinarySearch/BinarySearch.java
@@ -0,0 +1,23 @@
+package BinarySearch;
+
+public class BinarySearch {
+    static int binarySearch(int[] nums, int target){
+        int start = 0;
+        int end = nums.length-1;
+        while(start<=end){
+            int mid = start+(end-start)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]<target){
+                start = mid+1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        System.out.println(binarySearch(arr,5));
+    }
+}
