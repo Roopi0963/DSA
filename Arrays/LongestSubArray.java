6fd4ef9ba8fe15d9a8eda6b098c9f6e9970b8b35
PALEGARU ROOPESH NAIDU <palegariroopeshnaidu@gmail.com>
2025-08-14 21:12:47 +0530

    Add: Longest subarray .
    Implemented a method to return the longest subarray elements.

---
+++ Arrays/LongestSubArray.java
@@ -0,0 +1,28 @@
+package Arrays;
+
+public class LongestSubArray {
+    public static int getLongestSubarray(int []a, long k) {
+        int n = a.length; // size of the array.
+
+        int len = 0;
+        for (int i = 0; i < n; i++) { // starting index
+            for (int j = i; j < n; j++) { // ending index
+                // add all the elements of
+                // subarray = a[i...j]:
+                long s = 0;
+                for (int K = i; K <= j; K++) {
+                    s += a[K];
+                }
+
+                if (s == k)
+                    len = Math.max(len, j - i + 1);
+            }
+        }
+        return len;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 5, 1, 9};
+        System.out.println(getLongestSubarray(arr, 10));
+    }
+}
